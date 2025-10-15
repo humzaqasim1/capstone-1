31f52d2f8d4c1ca3f5213410bae3ca2fd35918a3
humza <humzaqasim1@gmail.com>
2025-10-15 10:11:14 -0500

    Created input switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,37 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
-//    FileReader fileReader = new FileReader("src/main/resources/transactions.csv");
+
+
+    //    FileReader fileReader = new FileReader("src/main/resources/transactions.csv");
 //    BufferedReader bufferedReader = new BufferedReader(fileReader);
     public static void main(String[] args) {
-        homeScreen();
+        inputSwitch();
 
 
     }
+
+    public static void inputSwitch() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            homeScreen();
+            String userChoice = scanner.nextLine().trim().toUpperCase();
+            switch (userChoice) {
+                case "A":
+                    break;
+                case "B":
+                    break;
+                case "C":
+                    break;
+                case "D":
+                    System.out.println("Exiting Application");
+                    return;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+
     private static void homeScreen() {
         System.out.println("\nWelcome to the Home Screen\n");
         System.out.println("Choose an Option: ");
